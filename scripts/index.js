945530f3568d7c1acf3334fd0b6636cd164bed0c
Pansage <43014518+Pansage@users.noreply.github.com>
2018-10-01 12:41:17 -0600

    aa
    aa

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function visitorCounter(visitor, times, startingNumber){
+let number = startingNumber
+  for (let i=1; i<= ; i++)
+  visitor+= visitor;
+  console.log(visitor);
+}
+
+visitorCounter(1,2,4)
